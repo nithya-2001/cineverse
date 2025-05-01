0bc0dea1402f653d25951e82ba145fc881d30359
Nithya Vardhan Reddy Aku <akunithya2001@gmail.com>
2025-05-01 17:26:07 -0500

    constants

---
+++ src/utils/constants.js
@@ -0,0 +1,18 @@
+export const logo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZrmyHdEXq3crIWfpomk6_DqIXqZByyhFg8w&s"
+
+export const avatar="https://cdn-icons-png.flaticon.com/512/9187/9187604.png"
+
+export const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMGY3OTgxMjRjNTRiMjUyZmNhZWM0ZGM1NTlmZDYwMyIsIm5iZiI6MTc0NjAzNTU4MS44MzMsInN1YiI6IjY4MTI2MzdkODg2YjQ1NThmNDA1NTNmYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sW_cIjKU4DRmW6dDmO4mHkhZkSNTBbrVO71p7coW4mc'
+    }
+  };
+
+export const IMG_CDN_URL="https://image.tmdb.org/t/p/w500"
+
+export const BG_URL="https://assets.nflxext.com/ffe/siteui/vlv3/9390f6f6-cf80-4bc9-8981-8c2cc8adf98a/web/US-en-20250421-TRIFECTA-perspective_d267de16-c801-48de-9014-f47514040d8b_small.jpg"
+
+
+export const OPENAI_KEY=process.env.REACT_APP_OPENAI_API_KEY;
